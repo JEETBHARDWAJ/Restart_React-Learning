31d11af9d51a5dc11af51ec245792d4b90d061e0
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-02-19 21:54:52 +0530

    neww

--- react_food/src/App.jsx
+++ react_food/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Food from "./component/Food";
 
 function App() {
   return <React.Fragment>
-
+    <Food></Food>
   </React.Fragment>
 }
 
--- react_food/src/component/Food.jsx
+++ react_food/src/component/Food.jsx
@@ -1,7 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 function Food(){
-    return(
-        
-    );
+  let Fooditems = [
+    "Fruit",
+    "Eggs",
+    "Meat",
+    "Nuts",
+    "Veggies",
+    "Seafood",
+    "Grains",
+    "Bread",
+  ];
+
+  return (
+    <>
+      <h1>Healty Food</h1>
+      <ul class="list-group">
+        {Fooditems.map(item=> <li class="list-group-item">{item}</li>)}
+      </ul>
+    </>
+  );
 }
 
 export default Food;
