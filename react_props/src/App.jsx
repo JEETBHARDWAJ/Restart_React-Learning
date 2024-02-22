de11166d321327ae059da8b2c9920c41eb7a1c8c
JEETBHADWAJ <bhardwajjeet408@gmail.com>
2024-02-22 20:02:47 +0530

    new

--- react_props/src/App.jsx
+++ react_props/src/App.jsx
@@ -1,10 +1,38 @@
+import Items from "./component/Items";
+import React from "react";
+
 function App() {
-  
+  let ar = [
+    "Pizza",
+    "Burger",
+    "Sushi",
+    "Pasta",
+    "Salad",
+    "Steak",
+    "Tacos",
+    "Curry",
+    "Fried Chicken",
+    "Ramen",
+    "Sandwich",
+    "Burrito",
+    "Pad Thai",
+    "Lasagna",
+    "Sushi",
+    "Dumplings",
+    "Fajitas",
+    "Soup",
+    "Wings",
+    "Nachos",
+  ];
+
   return (
     <>
-      
+      <center>
+        <h1>food Items</h1>
+      </center>
+      <Items item={ar}></Items>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
