e174a9f41abe9d71fc028e14d8f04d09d741bf6a
JEETBHARDWAJ <bhardwajjeet408@gmial.com>
2024-03-20 17:51:51 +0530

    Adding bootstrapping

--- react_socialMedia/src/App.jsx
+++ react_socialMedia/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import 'bootstrap/dist/css/bootstrap.css' 
+import Header from './component/Header';
+import Footer from './component/Footer';
+import SideBar from './component/SideBar';
+const app = ()=>{
+    return <>
+    <Header></Header>
+    <Footer></Footer>
+    <SideBar></SideBar>
+    </>;
 }
 
-export default App
+export  default app;
